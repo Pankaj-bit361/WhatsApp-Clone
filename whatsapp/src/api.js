71a579836b7b86f2fac7a5b1002c11c562d6997a
pankaj <pnkjvshsh1@gmail.com>
2023-10-08 17:14:12 +0530

    done

--- whatsapp/src/api.js
+++ whatsapp/src/api.js
@@ -1,11 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
-export const ApiUrl=`https://whats-app-clone-hazel.vercel.app`
+export const ApiUrl = `https://whats-app-clone-orpin.vercel.app`;
 
-export const getDate=(date)=>{
-  let gethours=new Date(date).getHours()
-  let getMin=new Date(date).getMinutes()
+export const getDate = (date) => {
+  let gethours = new Date(date).getHours();
+  let getMin = new Date(date).getMinutes();
 
-  return `${gethours<10?'0'+gethours:gethours}:${getMin<10?'0'+getMin:getMin}`
-}
-
+  return `${gethours < 10 ? "0" + gethours : gethours}:${
+    getMin < 10 ? "0" + getMin : getMin
+  }`;
+};
--- whatsapp/src/components/Allmesseges.jsx
+++ whatsapp/src/components/Allmesseges.jsx
@@ -1,51 +1,38 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import MessegeCard from './MessegeCard';
-import { ApiUrl } from '../api';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MessegeCard from "./MessegeCard";
+import { ApiUrl } from "../api";
 
-
-const Allmesseges = ({con,flag}) => {
-
+const Allmesseges = ({ con, flag }) => {
   const [messeges, setMesseges] = useState([]);
 
-
-
   const getMesseges = async () => {
-  
-
-  await axios
+    await axios
       .get(`${ApiUrl}/message/${con._id}`)
       .then((res) => {
-     
         setMesseges(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  };   
+  };
 
-  useEffect(()=>{
-  con._id && getMesseges()
-
-  },[con._id,flag])
-
-
+  useEffect(() => {
+    con._id && getMesseges();
+  }, [con._id, flag]);
 
   return (
+    <div className="bg-[url('https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png')] overflow-x-auto h-[80vh] w-[100%]">
+      <div className="pb-[5%]">
+        {messeges &&
+          messeges?.map((item) => (
+            <div key={item.createdAt} className="pl-[2%] pr-[2%]">
+              <MessegeCard {...item} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-     <div className="bg-[url('https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png')] overflow-x-auto h-[80vh] w-[100%]">
- <div className='pb-[5%]'>   
-{messeges &&  messeges?.map((item)=>(
-<div key={item.createdAt} className='pl-[2%] pr-[2%]'>
-  <MessegeCard  {...item} />
-</div>
-
-
-))}
-</div> 
-     </div>
-
-  )
-}
-
-export default Allmesseges
+export default Allmesseges;
