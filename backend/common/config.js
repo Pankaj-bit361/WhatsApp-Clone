e89a5aedd47c206373905f7736679ed97de8bf8f
pankaj <pankaj.v@questlabs.biz>
2024-09-22 23:23:55 +0530

    update config

--- backend/common/config.js
+++ backend/common/config.js
@@ -1,6 +1,5 @@
 const config ={
     GOOGLE_CLIENT_ID: "169162112976-ca9n5mfgi1o679cm251beuujids6rnii.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: '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',
 }
 
 module.exports = {
